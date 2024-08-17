ccff59a6217a88467a596f1d2412ea4ed4f1e8a3
Trgtuan10 <tuannguyentrong.402@gmail.com>
2024-08-17 22:39:56 +0700

    add unet

--- sd/attention.py
+++ sd/attention.py
@@ -51,3 +51,45 @@ class SelfAttention(nn.Module):
             output = self.out_proj(output)
 
             return output
+        
+class CrossAttention(nn.Module):
+
+    def __init__(self, n_heads: int, d_embed: int, d_cross: int, in_proj_bias=True, out_proj_bias=True):
+        super().__init__()
+        self.q_proj = nn.Linear(d_embed, d_embed, bias=in_proj_bias)
+        self.k_proj = nn.Linear(d_cross, d_embed, bias=in_proj_bias)
+        self.v_proj = nn.Linear(d_cross, d_embed, bias=in_proj_bias)
+
+        self.out_proj = nn.Linear(d_embed, d_embed, bias=out_proj_bias)
+        self.n_heads = n_heads
+        self.d_head = d_embed // n_heads
+
+    def forward(self, latent: torch.Tensor, context: torch.Tensor):
+        # latent: (Batch_Size, seq_length_q, dim_q)
+        # context: (Batch_Size, seq_length_k_v, dim_k_v) = (batch_size, 77, 768)
+
+        input_shape = latent.shape
+        batch_size, sequence_length, d_embed = input_shape
+
+        internim_shape = (batch_size, -1, self.n_heads, self.d_head)
+
+        #Multiquery by Wq
+        q = self.q_proj(latent)
+        k = self.k_proj(context)
+        v = self.v_proj(context)
+
+        weight = q @ k.transpose(-1, -2)
+
+        weight /= math.sqrt(self.d_head)
+
+        weight = F.softmax(weight, dim=-1)
+
+        output = weight @ v
+
+        output = output.transpose(1, 2).reshape(batch_size, sequence_length, d_embed)
+
+        output = self.out_proj(output)
+
+        return output
+
+        
--- sd/encoder.py
+++ sd/encoder.py
@@ -69,3 +69,38 @@ class VAE_Encoder(nn.Sequential):
             # (Batch_Size, 8, Height / 8, Width / 8) -> (Batch_Size, 8, Height / 8, Width / 8)
             nn.Conv2d(8, 8, kernel_size=1, padding=0), 
         )
+    
+    def forward(self, x, noise):
+        # x: (Batch_Size, Channel, Height, Width)
+        # noise: (Batch_Size, 4, Height / 8, Width / 8)
+
+        for module in self:
+
+            if getattr(module, 'stride', None) == (2, 2):  # Padding at downsampling should be asymmetric (see #8)
+                # Pad: (Padding_Left, Padding_Right, Padding_Top, Padding_Bottom).
+                # Pad with zeros on the right and bottom.
+                # (Batch_Size, Channel, Height, Width) -> (Batch_Size, Channel, Height + Padding_Top + Padding_Bottom, Width + Padding_Left + Padding_Right) = (Batch_Size, Channel, Height + 1, Width + 1)
+                x = F.pad(x, (0, 1, 0, 1))
+            
+            x = module(x)
+
+        # (Batch_Size, 8, Height / 8, Width / 8) -> two tensors of shape (Batch_Size, 4, Height / 8, Width / 8)
+        mean, log_variance = torch.chunk(x, 2, dim=1)
+        
+        # Clamp the log variance between -30 and 20, so that the variance is between (circa) 1e-14 and 1e8. 
+        # (Batch_Size, 4, Height / 8, Width / 8) -> (Batch_Size, 4, Height / 8, Width / 8)
+        log_variance = torch.clamp(log_variance, -30, 20)
+        # (Batch_Size, 4, Height / 8, Width / 8) -> (Batch_Size, 4, Height / 8, Width / 8)
+        variance = log_variance.exp()
+        # (Batch_Size, 4, Height / 8, Width / 8) -> (Batch_Size, 4, Height / 8, Width / 8)
+        stdev = variance.sqrt()
+        
+        # Transform N(0, 1) -> N(mean, stdev) 
+        # (Batch_Size, 4, Height / 8, Width / 8) -> (Batch_Size, 4, Height / 8, Width / 8)
+        x = mean + stdev * noise
+        
+        # Scale by a constant
+        # Constant taken from: https://github.com/CompVis/stable-diffusion/blob/21f890f9da3cfbeaba8e2ac3c425ee9e998d5229/configs/stable-diffusion/v1-inference.yaml#L17C1-L17C1
+        x *= 0.18215
+        
+        return x
